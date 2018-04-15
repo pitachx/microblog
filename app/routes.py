429bb753691a512f439c18880e6f5e88d00f1de9
Bers <pitachx@gmail.com>
2018-04-15 21:12:21 +0300

    back to the future

--- app/routes.py
+++ app/routes.py
@@ -26,7 +26,7 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-        posts = current_user.followed_posts().all()
+    posts = current_user.followed_posts().all()
     return render_template("index.html", title='Home Page', form=form, posts=posts)
 
 
