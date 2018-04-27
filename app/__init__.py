294f094a3e8c5a5c67a0b2881992bab61f4cd3b8
Bers <pitachx@gmail.com>
2018-04-27 14:57:15 +0300

    add bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -7,6 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
@@ -15,7 +18,9 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = "Пожалуйста, войдите, чтобы открыть эту страницу."
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
